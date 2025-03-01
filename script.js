14cb82c7cc3d6ab81b6ffc48b29b3b1651586bb6
Harshil5233 <127299744+Harshil5233@users.noreply.github.com>
2025-03-01 13:43:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,25 @@
-
 const navLinks = document.querySelectorAll('.nav-link');
-
 
 navLinks.forEach(link => {
     link.addEventListener('click', (e) => {
         e.preventDefault();
-
-
         const sections = document.querySelectorAll('.content-section');
         sections.forEach(section => {
             section.style.display = 'none';
         });
-
-
         const sectionId = e.target.getAttribute('data-section');
         const sectionToShow = document.getElementById(sectionId);
         sectionToShow.style.display = 'block';
     });
 });
 
-
 const readMoreBtns = document.querySelectorAll('.read-more');
 readMoreBtns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
         e.preventDefault(); 
-
         const post = e.target.closest('.post');
         const fullContent = post.querySelector('.full-content');
-        
-   
         fullContent.style.display = 'block'; 
-
-      
         e.target.style.display = 'none'; 
     });
 });
-
